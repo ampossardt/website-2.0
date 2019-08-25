d614591b173575d0e91c41d1540eec7852faead9
Andrew Possardt <andrewmpossardt@gmail.com>
2019-08-25 10:37:59 -0400

    Moving to desktop

--- client/src/components/about/about.js
+++ client/src/components/about/about.js
@@ -18,7 +18,14 @@ class About extends React.Component {
     return (
       <div className={`container about ${(this.state.hide || '') && 'hide'}`}>
         <svg className="top-left" viewBox={`0 0 1600 900`} style={ { width: window.innerWidth / 2, height: (window.innerWidth / 2) * (9/16) } }>
-          <polygon points="0 0, 0 100, 100 0" fill="#fff" />
+          <polygon points="0 0, 100 0, 0 100" fill="#fff" />
+
+          <polygon points="0 100, 50 50, 70 50, 0 120" fill="#951943" />
+          <polygon points="50 50, 100 0, 120 0, 70 50" />
+
+          <polygon points="0 120, 40 80, 80 80, 0 160" />
+          <polygon points="40 80, , 80 80, 0 160" />
+
           <polygon points="1000 1000" fill="transparent" />
         </svg>
         <button onClick={ () => this.hide() }>close</button>
