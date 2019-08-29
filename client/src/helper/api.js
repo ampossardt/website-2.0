1331d24dda65acc0de89d90eac6c2420396ef1ba
Andrew Possardt <andrewmpossardt@gmail.com>
2019-08-28 21:08:42 -0400

    Need to know what environment heroku thinks im deploying

--- client/src/helper/api.js
+++ client/src/helper/api.js
@@ -2,8 +2,6 @@
 import { config } from '../assets/constants';
 
 export const getPhotos = () => {
-
-  console.log(config.apiUrl);
   return fetch(`${config.apiUrl}/photos`)
     .then(response => response.json());
 }
