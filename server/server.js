99957a3a8f23ce5f21c47582f016c433d1eb4e9c
Andrew Possardt <andrewmpossardt@gmail.com>
2019-08-28 21:26:11 -0400

    Forgot to listen for port from heroku

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3500;
+const port = process.env.PORT || 3500;
 const Flickr = require('flickr-sdk');
 const fetch = require('node-fetch');
 
