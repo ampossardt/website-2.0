b230457834db2f778053a711be2d055cec98e3e8
Andrew Possardt <andrewmpossardt@gmail.com>
2019-08-28 21:00:55 -0400

    debugging

--- client/src/helper/api.js
+++ client/src/helper/api.js
@@ -2,6 +2,8 @@
 import { config } from '../assets/constants';
 
 export const getPhotos = () => {
+
+  console.log(config.apiUrl);
   return fetch(`${config.apiUrl}/photos`)
     .then(response => response.json());
 }
